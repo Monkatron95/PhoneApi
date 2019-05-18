127360355d14a0c020f2345c4624bbe71468f340
Joshua Monk <josh-monk1995@outlook.com>
2019-05-18 09:35:27 +0100

    Init project from spring boot initaliser

---
+++ src/main/java/com/joshuamonk/phoneapi/PhoneapiApplication.java
@@ -0,0 +1,13 @@
+package com.joshuamonk.phoneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joshuamonk/phoneapi/PhoneapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.joshuamonk.phoneapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhoneapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
