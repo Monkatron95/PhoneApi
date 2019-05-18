70ae2c4ddb1e51ee172406a18156caa6c1794ce7
Joshua Monk <josh-monk1995@outlook.com>
2019-05-18 11:59:24 +0100

    Implemented get all phone numbers functionality

--- src/main/java/com/joshuamonk/phoneapi/service/CustomerService.java
+++ src/main/java/com/joshuamonk/phoneapi/service/CustomerService.java
@@ -1,7 +1,25 @@
 package com.joshuamonk.phoneapi.service;
 
+import com.joshuamonk.phoneapi.model.repo.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+/**
+ * Service for handling phone numbers in the system
+ */
 @Service
 public class CustomerService {
+
+    @Autowired
+    private CustomerRepo customerRepo;
+
+    /**
+     * Gets all of the phone numbers currently associated with a customer in the system
+     * @return  List<String> containing all numbers in the system
+     */
+    public List<String> getAllNumbers() {
+        return this.customerRepo.getAllNumbers();
+    }
 }
